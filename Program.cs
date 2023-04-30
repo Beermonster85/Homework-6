bf9771bb48006ccd4fac63734f4fd9e385f25e07
Kirill <kiril-shitikov@yandex.ru>
2023-04-30 15:43:03 +0300

    docs: HW task #2

--- Program.cs
+++ Program.cs
@@ -79,6 +79,7 @@
 
 
 // Homework
+// Задание №1
 // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
@@ -95,3 +96,24 @@
 // }
 
 // Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
+// Задание №2
+//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.WriteLine("введите значение b1");
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// double x = (-b2 + b1) / (-k1 + k2);
+// double y = k2 * x + b2;
+
+// Console.Write(x);
+// Console.WriteLine(y);
