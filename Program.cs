9ad6f9c961e4ee53f6174c51275841fdd4dab0eb
Kirill <kiril-shitikov@yandex.ru>
2023-04-30 15:24:17 +0300

    docs:HW task #1

--- Program.cs
+++ Program.cs
@@ -75,3 +75,23 @@
 // }
 
 // Console.WriteLine(String.Join("", array));
+
+
+
+// Homework
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int[] array = new int[5] { 1, 2, 0, 4, 5 };
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
